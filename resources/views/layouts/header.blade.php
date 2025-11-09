38f17ba9b52a30be7cbab12a8f64cfc6d55cf0b3
readonegate <ridwan.gate@gmail.com>
2025-11-09 11:31:35 +0700

    Add login route

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -25,10 +25,20 @@
              <a class="text-gray-500 transition hover:text-gray-500/75" href="{{ route('blog.profile') }}">
                Profil </a>
            </li>
-
-           </li>
          </ul>
        </nav>
+       <div class="flex items-center gap-4">
+         <div class="sm:flex sm:gap-4">
+           <a class="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
+             href="{{ route('auth.login') }}">
+             Login
+           </a>
+
+           {{-- <a class="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
+             href="#">
+             Register
+           </a> --}}
+         </div>
+       </div>
      </div>
-   </div>
  </header>
